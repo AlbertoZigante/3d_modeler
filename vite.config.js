117c840c504212adc23b32ab192830b4ce7a2aa7
AlbertoZigante <alberto.zigante@gmail.com>
2026-07-05 16:37:39 +0200

    Add modeller.html as a fuild entry point

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,14 @@
-export default {
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
   base: '/3d_modeler/',
-}
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        modeller: resolve(__dirname, 'modeller.html'),
+      },
+    },
+  },
+})
